3c66b1747c149463895f37da0bf938759eb21001
Edgar Ehrenberg <ainshin@its.jnj.com>
2021-01-27 21:46:51 +0300

    Project Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
-import "./App.css";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
